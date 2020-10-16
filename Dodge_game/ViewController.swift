88299746a4ddb1dd40a7e7e3c28b2026adcd2d6f
Do Hyeong Seok <zxc1460@naver.com>
2020-10-16 21:04:05 +0900

    Initial Commit

---
+++ Dodge_game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dodge_game
+//
+//  Created by Seok on 2020/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
